deb2893c4d5761d5235774a49d6f083ba7dc1946
Azrin640 <azrin640@gmail.com>
2019-04-01 01:51:14 +0800

    Add folders

--- ngwp-mysql/sys/x@0024session.frm
+++ ngwp-mysql/sys/x@0024session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2019-03-30 19:49:22
+timestamp=2019-03-31 15:20:29
 create-version=1
 source=SELECT * FROM sys.x$processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
--- ngwp-wp/wp-config.php
+++ ngwp-wp/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wp_db');
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress');
+define( 'DB_USER', 'wp_user');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wordpress');
+define( 'DB_PASSWORD', 'Engine1@');
 
 /** MySQL hostname */
 define( 'DB_HOST', 'ngwp-mysql:3306');
